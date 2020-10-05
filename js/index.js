3f8c68d60833d4d2617a322a6dc9d6eb5856dcb5
[Students] Maris Ingus Klava <91DPMIKlava@rvt.lv>
2020-10-05 16:17:43 +0300

    Viss

---
+++ js/index.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function(){
+    
+})
+
+//Konvertē cilvēkam lasāmu adresi precīzās koordinātās
+//Izmantoju bezmaksas geokodēšanas api http://photon.komoot.de/api/*
+async function getLocation(location) {
+    return new Promise(async (resolve, reject) => {
+        let res = await (await fetch("http://photon.komoot.de/api/?q="+location)).json()
+        resolve(res.features[0].geometry.coordinates)
+    })
+}
+
+async function getWeather() {
+    let coordinates = await getLocation("Riga, Latvia")
+    let res = await (await fetch("http://photon.komoot.de/api/?q="+location)).json()
+
+}
