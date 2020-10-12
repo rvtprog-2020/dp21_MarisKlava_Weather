dd124914ca9d1c730cd941abc7eeca7488210afb
[Students] Maris Ingus Klava <91DPMIKlava@rvt.lv>
2020-10-12 11:24:25 +0300

    Almost done

--- js/index.js
+++ js/index.js
@@ -1,19 +1,45 @@
-window.addEventListener("load", function(){
-    
+window.addEventListener("load", async function(){
+    let weather = await getWeather()
+
+    let area = weather.nearest_area[0]
+    let areaName = area.areaName[0].value
+    let region = area.region[0].value
+    let country = area.country[0].value
+
+    let condition = weather.current_condition[0]
+    let TempC = condition.temp_C
+    let TempF = condition.temp_F
+    let date = condition.localObsDateTime
+    let weatherDesc = condition.weatherDesc[0].value
+
+    document.getElementById("location").innerHTML = "Weather for " + areaName + ", " + region + ", " + country
+    document.getElementById("temperature").innerHTML = TempC + "°C | " + TempF + "°F"
+    document.getElementById("weatherdate").innerHTML = date
+    document.getElementById("condition").innerHTML = weatherDesc
 })
 
 //Konvertē cilvēkam lasāmu adresi precīzās koordinātās
 //Izmantoju bezmaksas geokodēšanas api http://photon.komoot.de/api/*
-async function getLocation(location) {
+/*async function getLocation(location) {
     return new Promise(async (resolve, reject) => {
-        let res = await (await fetch("http://photon.komoot.de/api/?q="+location)).json()
-        resolve(res.features[0].geometry.coordinates)
+        try {
+            let res = await (await fetch("http://photon.komoot.de/api/?q="+location)).json()
+            resolve(res.features[0].geometry.coordinates)
+        }
+        catch (err) {
+            reject(err)
+        }
     })
-}
+}*/
 
 async function getWeather() {
-    let coordinates = await getLocation("Riga, Latvia")
-    let res = await (await fetch("http://photon.komoot.de/api/?q="+location)).json()
-
-    
+    return new Promise(async (resolve, reject) => {
+        try {
+            let res = await (await fetch("http://wttr.in/?format=j1")).json()
+            resolve(res)
+        }
+        catch (err) {
+            reject(err)
+        }
+    })
 }
